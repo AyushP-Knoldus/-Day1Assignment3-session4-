8629a74e3c5dc860af11501d17f45266d8bf43ad
Ayush Pathak <125042596+AyushP-Knoldus@users.noreply.github.com>
2023-03-29 18:51:35 +0530

    Update ElementInList.scala

--- src/main/scala/ElementInList.scala
+++ src/main/scala/ElementInList.scala
@@ -1,16 +1,17 @@
 package com.knoldus
+
 //Generic type class with method elementKFromEnd which takes generic type list
 class ElementInList[T] {
   //elementKFromEnd method returns kth element form the end of the list
-  def elementKFromEnd(list : List[T] , kthElement : Int) : Option[T] = {
+  def elementKFromEnd(list: List[T], kthElement: Int): Option[T] = {
     //Exception is being handled using if else statement
-      val sizeOfList= list.size
-      if(sizeOfList > kthElement) {
-        val elementFromLeft = sizeOfList - kthElement
-        Some(list(elementFromLeft))
-      } else {
-        None
-      }
+    val sizeOfList = list.size
+    if (sizeOfList > kthElement) {
+      val elementFromLeft = sizeOfList - kthElement
+      Some(list(elementFromLeft))
+    } else {
+      None
+    }
   }
 }
 
